85a5821ae517bc5186867975423352227c711db2
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-17 23:59:12 +0100

    Add counter reducer

---
+++ src/useReducer/counter_exersice/counterReducer.js
@@ -0,0 +1,14 @@
+const initialCount = 0
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1
+    case "decrement":
+      return state - 1
+    case "reset":
+      return 0
+    default:
+      return state
+  }
+}
+export { initialCount, counterReducer }
