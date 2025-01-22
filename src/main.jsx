ce42f821eb6d0169e43e35de7e27a125feba2334
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-22 01:44:15 +0100

    Refactor state management logic in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-// import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
-import { Toggle } from "./useReducer/toggle_exersice/toggle";
-import { Counter } from "./useReducer/counter_exersice/counter";
-import { Card } from './useReducer/shoping_exersices/Card'
-createRoot(document.getElementById("root")).render(
-  // <Provider store={store}>
-  <>
-    <Toggle />
-    <hr />
-    <Counter />
-    <hr />
-    <Card />
-  </>
-  // </Provider>
-);
+import { createStore } from "redux";
+import { Card } from "./useReducer/shoping_exersices/Card";
+import { CounterReducer } from "./redux/Reducers/counterReducer";
+const store = createStore(CounterReducer);
+store.subscribe(() => {
+  alert("the state has been changed !!!!! "+ store.getState().value);
+});
+for (let index = 0; index < 10; index++) {
+  store.dispatch({ type: "increment"});
+}
+// createRoot(document.getElementById('root')).render(
+//   <Card />
+// )
