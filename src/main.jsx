e25a41c75d47d460eb59f388107346aa450db3ec
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-23 01:22:45 +0100

    Update project dependencies and main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { createStore } from "redux";
-import { Card } from "./useReducer/shoping_exersices/Card";
-import { CounterReducer } from "./redux/Reducers/counterReducer";
-const store = createStore(CounterReducer);
-store.subscribe(() => {
-  alert("the state has been changed !!!!! "+ store.getState().value);
-});
-for (let index = 0; index < 10; index++) {
-  store.dispatch({ type: "increment"});
-}
-// createRoot(document.getElementById('root')).render(
-//   <Card />
-// )
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { App } from "./redux-toolkit/App";
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
